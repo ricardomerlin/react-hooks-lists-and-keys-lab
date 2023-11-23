59d4f5c4e92738eb369b6a52b48a1234bd43d2a8
Ricardo Merlin <rcrdmerlin@gmail.com>
2023-11-22 19:12:12 -0500

    Uploading react hooks lists and keys lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const mapLinks = links.map(item => {
+    return <a href={'#' + item} key={item}>{item}</a>
+  })
+
+  return <nav>{mapLinks}</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+
+  let mapTech = technologies.map(item => (
+    <span key={item}>{item}</span>
+  ))
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {mapTech}
       </div>
     </div>
   );
